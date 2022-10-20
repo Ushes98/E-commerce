8c8f51075c117be0f9288ce5a7537fd8715a535d
Ushes98 <107640788+Ushes98@users.noreply.github.com>
2022-10-20 21:17:47 +0545

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,26 @@
+$('.owl-carousel').owlCarousel({
+  loop: true,
+  margin: 30,
+  responsiveClass: true,
+  nav: false,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 2,
+    },
+    1000: {
+      items: 4,
+      loop: true,
+    }
+  }
+})
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+      });
+  });
+});
